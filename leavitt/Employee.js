b27a4404c5041311c9730266bf22610772134fa5
Jared Barnes <jared.j.barnes@gmail.com>
2011-12-08 07:55:54 -0700

    Fixes for IE

--- leavitt/Employee.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="/scripts/leavitt/Person.js" />
-
-BASE.require(["leavitt.Person"], function () {
-    Object.namespace("leavitt");
-    leavitt.Employee = function () {
-        leavitt.Person.apply(this, arguments);
-    };
-    leavitt.Employee.prototype = new leavitt.Person();
-});
