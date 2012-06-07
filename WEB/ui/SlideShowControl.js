2a7e1409cf53e8edfc27525f32189a211a5561e4
unknown <SUU@bnp3wc1.(none)>
2012-06-07 00:45:18 -0600

    add some view controls and updated executeDataScript.js

---
+++ WEB/ui/SlideShowControl.js
@@ -0,0 +1,40 @@
+﻿BASE.require(["jQuery"], function () {
+    BASE.namespace("WEB.ui");
+
+    WEB.ui.SlideShowControl = function (elem) {
+        if (!(this instanceof WEB.ui.SlideShowControl)) {
+            return new WEB.ui.SlideShowControl(elem);
+        }
+
+        var self = this;
+        var $elem = $(elem);
+        var $stop = $elem.find("#stop");
+        var $start = $elem.find("#start");
+        var $next = $elem.find("#next");
+        var $prev = $elem.find("#previous");
+        var $slideShow = $elem.find("[data-script='WEB.ui.SlideShow']");
+        var slideShow = $slideShow.length > 0 ? $slideShow[0].controller : null;
+
+        if (!slideShow) {
+            $slideShow.one("controlReady", function () {
+                slideShow = $slideShow.length > 0 ? $slideShow[0].controller : null;
+            });
+        }
+
+        $stop.bind("click", function (e) {
+            if (slideShow) slideShow.stop();
+        });
+
+        $start.bind("click", function (e) {
+            if (slideShow) slideShow.start();
+        });
+
+        $next.bind("click", function (e) {
+            if (slideShow) slideShow.next();
+        });
+
+        $prev.bind("click", function (e) {
+            if (slideShow) slideShow.prev();
+        });
+    };
+});
--- jQuery/executeDataScript.js
+++ jQuery/executeDataScript.js
@@ -1,21 +1,48 @@
 BASE.require(["jQuery"], function () {
 
     jQuery.executeDataScript = function (root) {
-        $("[data-script]", root || document).each(function () {
-            var $this = $(this);
-            var script = $this.attr("data-script");
-            if (!$this.data("data-script")) {
-                BASE.require([script], function () {
-                    var Klass = BASE.getObject(script);
+        var scriptArray = [];
+        var $elemArray = [];
 
-                    if (typeof Klass === "function") {
-                        $this[0].controller = new Klass($this[0]);
-                        var event = new $.Event("controlReady");
-                        $this.trigger(event);
-                    } else {
-                        throw new Error("\"" + script + "\" needs to be a class.");
-                    }
-                });
+        var exec = function ($this) {
+            var script = $this.attr("data-script");
+            if (script) {
+                scriptArray.push(script);
+                $elemArray.push($this);
+            }
+        };
+
+        var walk = function ($root) {
+            var $children = $root.children();
+            var $child;
+            for (var x = 0 ; x < $children.length; x++) {
+                $child = $children.slice(x, x + 1);
+                walk($child);
+            }
+            exec($root);
+        };
+
+        walk($(root || document.body));
+        BASE.require(scriptArray, function () {
+            var Klass;
+            var $this;
+            var script;
+            var i;
+
+            while (scriptArray.length > 0) {
+                script = scriptArray.pop();
+                $this = $elemArray.pop();
+
+                Klass = BASE.getObject(script);
+
+                if (typeof Klass === "function") {
+                    i = scriptArray.length;
+                    $this[0].controller = new Klass($this[0]);
+                    var event = new $.Event("controlReady");
+                    $this.trigger(event);
+                } else {
+                    throw new Error("\"" + script + "\" needs to be a class.");
+                }
             }
         });
     };
