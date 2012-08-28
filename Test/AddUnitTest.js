72aa4fe7e746e76dadb65f5f1986b369b947c413
Jared Barnes <jared.j.barnes@gmail.com>
2012-08-27 20:29:16 -0600

    Put all events on AddUnitTest.js

--- Test/AddUnitTest.js
+++ Test/AddUnitTest.js
@@ -6,8 +6,24 @@
         BASE.EventEmitter.call(this);
 
         self.run = function () {
+            var error = new BASE.Event("error");
+            error.message = "We have an error!";
+
+            self.emit(error);
+
+            var warning = new BASE.Event("warning");
+            warning.message = "This is a warning, I'm counting to three!";
+
+            self.emit(warning);
+
+            var notes = new BASE.Event("notes");
+            notes.message = "Some Notes of sorts";
+
+            self.emit(notes);
+
             var event = new BASE.Event("success");
             event.message = "It Worked!";
+            event.details = "This is a longer test explaining why this worked and that we all succeeded."
 
             self.emit(event);
         };
