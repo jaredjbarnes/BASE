6be1273851e2c780ff624a409ec00521cbe90913
unknown <jared-barnes@jareds-pc.leavitt.com>
2012-12-27 15:01:15 -0700

    update

--- jQuery/fn/loadModule.js
+++ jQuery/fn/loadModule.js
@@ -1,29 +1,35 @@
 
-BASE.require(["jQuery","jQuery.loadFile","jQuery.executeDataScript"], function(){
-    jQuery.fn.loadModule = function(url, options){
+BASE.require(["jQuery", "jQuery.loadFile", "WEB.MVC"], function () {
+    jQuery.fn.loadModule = function (url, options) {
         options = options || {};
-        var beforeAppend = options.beforeAppend || function(){};
-        var afterAppend = options.afterAppend || function(){};
-        var err = options.error || function(){
-            throw new Error("Couldn't find module located at \""+url+"\".");    
+        var beforeAppend = options.beforeAppend || function () { };
+        var afterAppend = options.afterAppend || function () { };
+        var prepend = options.prepend || false;
+        var err = options.error || function () {
+            throw new Error("Couldn't find module located at \"" + url + "\".");
         };
-        return this.each(function(){
+        return this.each(function () {
             var $this = $(this);
             $.loadFile(url, {
                 fromCache: true,
-                success:function(html){
+                success: function (html) {
                     var $module = $(html);
-                    beforeAppend.call($this[0], $module[0]);
-                    $module.appendTo($this);
-                    jQuery.executeDataScript($this[0]);
-                    afterAppend.call($this[0], $module[0]);
+                    WEB.MVC.applyTo($module[0], function () {
+                        beforeAppend.call($this[0], $module[0]);
+                        if (prepend) {
+                            $module.prependTo($this);
+                        } else {
+                            $module.appendTo($this);
+                        }
+                        afterAppend.call($this[0], $module[0]);
+                    });
                 },
-                error: function(){
+                error: function () {
                     err.call($this[0]);
                 }
-            }, function(){});
+            }, function () { });
         });
     };
-     
+
 
 });
