df151ce3d9c70262ce2647dddf32902d6b40b2c1
unknown <jared-barnes@jareds-pc.leavitt.com>
2013-01-07 09:58:27 -0700

    updates

--- jQuery/fn/loadModule.js
+++ jQuery/fn/loadModule.js
@@ -4,23 +4,28 @@ BASE.require(["jQuery", "jQuery.loadFile", "WEB.MVC"], function () {
         options = options || {};
         var beforeAppend = options.beforeAppend || function () { };
         var afterAppend = options.afterAppend || function () { };
-        var prepend = options.prepend || false;
+
         var err = options.error || function () {
             throw new Error("Couldn't find module located at \"" + url + "\".");
         };
         return this.each(function () {
             var $this = $(this);
+            // This allows custom dom attachment. 
+            // If you want to attach it somewhere in the parent besides the end.
+            var attachToDom = options.attachToDom || function (parent, module) {
+                var $this = $(parent);
+                var $module = $(module);
+
+                $module.appendTo($this);
+            };
+
             $.loadFile(url, {
                 fromCache: true,
                 success: function (html) {
                     var $module = $(html);
                     WEB.MVC.applyTo($module[0], function () {
                         beforeAppend.call($this[0], $module[0]);
-                        if (prepend) {
-                            $module.prependTo($this);
-                        } else {
-                            $module.appendTo($this);
-                        }
+                        attachToDom($this[0], $module[0]);
                         afterAppend.call($this[0], $module[0]);
                     });
                 },
