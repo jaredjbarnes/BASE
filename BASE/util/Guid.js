8c26cb17e8a9ce2651d44ef6114eba8aee238882
Jared Barnes <jared.j.barnes@gmail.com>
2014-01-30 23:38:45 -0700

    Added Guid

---
+++ BASE/util/Guid.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    BASE.namespace("BASE.util");
+
+    var S4 = function () {
+        return Math.floor(
+                Math.random() * 0x10000 /* 65536 */
+            ).toString(16);
+    };
+
+    BASE.util.Guid = {
+        create: function () {
+            return (
+                    S4() + S4() + "-" +
+                    S4() + "-" +
+                    S4() + "-" +
+                    S4() + "-" +
+                    S4() + S4() + S4()
+                );
+        }
+    };
+
+
+})();
